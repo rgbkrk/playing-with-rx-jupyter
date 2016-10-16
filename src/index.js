fa34e3517666c2be3b016feed801740c257092fb
Kyle Kelley <rgbkrk@gmail.com>
2016-10-16 10:50:23 -0700

    use rx-jupyter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import 'rxjs/add/operator/map';
+
+const rxJupyter = require('rx-jupyter');
+
+const jupyter = new rxJupyter.JupyterAPI("http://localhost:8888", true);
+
+window.jupyter = jupyter;
+
+jupyter.listKernelspecs()
+  .subscribe(x => {
+    ReactDOM.render(
+      <App data={JSON.stringify(x.response, 2, 2)} />,
+      document.getElementById('root')
+    );
+  })
