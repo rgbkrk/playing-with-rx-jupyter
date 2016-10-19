e350d9e13b816ad2cd7f72818d01e51cc8ac784d
Kyle Kelley <rgbkrk@gmail.com>
2016-10-18 19:33:49 -0700

    yar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/pluck';
+
+const uuid = require('uuid');
+
+import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject';
+import { webSocket } from 'rxjs/observable/dom/webSocket';
+
+window.webSocket = webSocket;
+window.WebSocketSubject = WebSocketSubject;
+
+const session = uuid.v4();
+
+export function createMessage(msg_type, fields) {
+  const username = 'kylek';
+  return Object.assign({
+    header: {
+      username,
+      session,
+      msg_type,
+      msg_id: uuid.v4(),
+      date: new Date(),
+      version: '5.0',
+    },
+    metadata: {},
+    parent_header: {},
+    content: {},
+  }, fields);
+}
+
+
+export function createExecuteRequest(code) {
+  const executeRequest = createMessage('execute_request');
+  executeRequest.content = {
+    code,
+    silent: false,
+    store_history: true,
+    user_expressions: {},
+    allow_stdin: false,
+    stop_on_error: false,
+  };
+  executeRequest.msg_type = executeRequest.header.msg_type;
+  executeRequest.msg_id = executeRequest.header.msg_id;
+  executeRequest.channel = 'shell';
+  executeRequest.buffers = [] ;
+
+  return executeRequest;
+}
+
+const ws = webSocket('ws://localhost:8888/api/kernels/4ce72033-e7f6-4b25-ba4b-0b8b72f7668c/channels')
+
+ws.subscribe(console.log.bind(console));
+
+window.createExecuteRequest = createExecuteRequest;
+
+window.ws = ws;
+
+window.createMessage = createMessage;
 
 const jupyter = require('rx-jupyter');
-
-window.jupyter = jupyter;
 
 const serverConfig = {
   endpoint: "http://localhost:8888",
   crossDomain: true,
 };
 
-jupyter.kernelspecs.list(serverConfig)
-  .subscribe(x => {
+const formWebSocketURL = (id) => {
+  return `${serverConfig.endpoint}/api/kernels/${id}/channels`;
+}
+
+window.formWebSocketURL = formWebSocketURL;
+window.jupyter = jupyter;
+window.serverConfig = serverConfig;
+
+jupyter.kernels.list(serverConfig)
+  .map(x => x.response)
+  .subscribe(kernels => {
     ReactDOM.render(
-      <App data={JSON.stringify(x.response, 2, 2)} />,
+      <div>
+        { kernels.map(kernel =>
+          <pre key={kernel.id}>{formWebSocketURL(kernel.id)}</pre>
+        )}
+      </div>,
       document.getElementById('root')
     );
-  })
+  },
+  () => console.error('hey'))
